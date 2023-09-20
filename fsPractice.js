4c402a594d90ab5d6fd9b6a3b6802acef17a4c66
Victor P <mfreke.victor@gmail.com>
2023-09-20 23:15:28 +0100

    Ignored fs practice

--- fsPractice.js
+++ fsPractice.js
@@ -1,82 +1,66 @@
-//To create a folder named "Frontend-scaffold"
-
 const fs = require("fs");
 
-fs.mkdir("frontend-scaffold",function(err){
-        if(err){console.log(err);}
-
-        // To create an index.html file inside frontend-scaffold
-        fs.open("./frontend-scaffold/index.html", 'w',(err)=>{
-                if(err){
-                    console.log(err);
-                }
-            
-                console.log("Index.html created successfully");
-            })
-
-        // To create css folder inside frontend-scaffold
-
-        fs.mkdir("./frontend-scaffold/css",function(err){
-                if(err){
-                    console.log(err);
-                }
-
-                const content = "h1{text-align:center}";
-
-        //To create style.css inside CSS Folder inisde rontend-scaffold
-
-                fs.writeFile("./frontend-scaffold/css/style.css",content,function(err){
-                            if(err){
-                                console.log(err);
-                            }
-
-                             console.log("style.css created successfully");
-                        })
-                
-                            console.log("Css folder created successfully");
-                        })
-
-
-        // To create a folder named js folder
-
-        fs.mkdir("./frontend-scaffold/js",function(err){
-            if(err){
-                console.log(err);
-            }
+const indexContent = `<!DOCTYPE html>
+  
+  <html lang="en">
+  
+    <head>
+    
+        <meta charset="UTF-8">
         
-            console.log("js folder created successfully");
-
-            // To create script.js inside js folder
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <link rel="stylesheet" href="./css/style.css">
+        
+        <title>Document</title>
     
-            const contain = "alert(welcome)";
+    </head>
     
-            fs.writeFile("./frontend-scaffold/js/script.js",contain,function(err){
-                    if(err){
-                        console.log(err);
-                    }
-                
-                    console.log("script.js created successfully");
-                })
-        })
-
-
-        // To create images folder
-
-        fs.mkdir("./frontend-scaffold/images",function(err){
-            if(err){
-                console.log(err);
-            }
-
-            fs.open("./frontend-scaffold/images/.gitkeep","w",function(err){
-                if(err){
-                    console.log(err);
-                }
-            
-                console.log("empty images file created successfully")
-            })
-
-            console.log("images folder created successfully");
-        })
+    <body>
     
-        console.log(" Frontend-Scaffold Folder created successfully");
-    })
+        <h1>Welcome</h1>
+        
+        <script src="./js/script.js"></script>
+    
+    </body>
+    
+  </html>`;
+
+const cssContent = `h1 {
+    text-align:center;
+  }`;
+
+const jsContent = `alert("Welcome");`;
+
+const fontendScaffold = () => {
+  try {
+    // Create a folder
+    fs.mkdirSync("frontend-scaffold");
+
+    // create a html file inside the folder
+    fs.writeFileSync("./frontend-scaffold/index.html", indexContent);
+
+    // Create css folder
+    fs.mkdirSync("frontend-scaffold/css");
+
+    // create css file inside the folder
+    fs.writeFileSync("./frontend-scaffold/css/style.css", cssContent);
+
+    // Create javascript folder
+    fs.mkdirSync("frontend-scaffold/js");
+
+    // create javaScript file inside the folder
+    fs.writeFileSync("./frontend-scaffold/js/script.js", jsContent);
+
+    // create an images folder
+    fs.mkdirSync("frontend-scaffold/images");
+
+    console.log("Frontend scaffold created successfully!");
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+fontendScaffold();
